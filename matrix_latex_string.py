abdba3f77727863692dbfe6235147b2943974a74
numairmansur <numair.mansur@gmail.com>
2016-03-19 16:03:37 +0100

    latexstring

--- matrix_latex_string.py
+++ matrix_latex_string.py
@@ -3,14 +3,11 @@ Latex matrix string generator
 '''
 
 def latex_matrix_string(mean,error,title, row_labels, col_labels, best_bold_row=True, best_bold_column=False):
-	#check 1:   dimensions of mean and error should be equal.
-	#check 2:   dimension of rows must be equal to dimension of the mean
-	#check 3:   dmension of columns must be equsl to the number of elements in one element of the mean.
 	matrix_string ='''\hline 
 '''
 	for i,row in enumerate(mean):
 		column_string ='''{ |c'''
-		matrix_string= matrix_string+"\\textbf{"+row_labels[i]+"}& "
+		matrix_string= matrix_string+"\\textbf{"+row_labels[i]+"}& " #length of row labels and number of rows must be equal
 		for j,cell in enumerate(row):
 			column_string = column_string + '''|c'''
 			ending_string = ''' & ''' if j < len(row)-1 else ''' \\\ \hline
@@ -47,5 +44,4 @@ error=[[2,6,1,5],[4,8,2,3],[1,4,8,2]]
 print( latex_matrix_string(mean, error,"Testing Testing", ["row1","row2","row3"], ["col1","col2","col3","col4"]) )
 
 
-#TOdo: 
-# 5) Add docstring in the same format as in ROBO.
+#TODO: aad robo style docdtrings
