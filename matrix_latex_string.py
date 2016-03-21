d0da198bd7a1b1d65c9041865366943e715a62eb
numairmansur <numair.mansur@gmail.com>
2016-03-21 09:38:55 +0100

    update

--- matrix_latex_string.py
+++ matrix_latex_string.py
@@ -1,47 +1,78 @@
-'''
-Latex matrix string generator
-'''
+def latex_matrix_string(
+        mean,
+        error,
+        title,
+        row_labels,
+        col_labels,
+        best_bold_row=True,
+        best_bold_column=False):
+    '''
+Latex Matrix String Generator.
 
-def latex_matrix_string(mean,error,title, row_labels, col_labels, best_bold_row=True, best_bold_column=False):
-	matrix_string ='''\hline 
+Parameters
+----------
+mean : array of float array
+        An array of float arrays containing mean values
+error : array of float array
+        An array of float array containing error values
+title : string
+        Title string of the table
+row_labels : string array
+        Array of strings for row names
+col_labels : string arrays
+        Array of strings for column names
+best_bold_row : boolean
+        If set to true, the minimum mean entry in each row will
+        be set to bold.
+best_bold_column :
+        If set to true, the minimum mean entry in each column will
+        be set to bold.
 '''
-	for i,row in enumerate(mean):
-		column_string ='''{ |c'''
-		matrix_string= matrix_string+"\\textbf{"+row_labels[i]+"}& " #length of row labels and number of rows must be equal
-		for j,cell in enumerate(row):
-			column_string = column_string + '''|c'''
-			ending_string = ''' & ''' if j < len(row)-1 else ''' \\\ \hline
+    matrix_string = '''\hline
 '''
-			if best_bold_row and cell == min(row) and best_bold_column == False:
-				matrix_string = matrix_string +"$\mathbf{" + str(cell)+" \pm "+str(error[i][j])+"}$"+ ending_string
-			elif best_bold_column and cell == min([a[j] for a in mean]) and best_bold_row == False:
-				matrix_string = matrix_string +"$\mathbf{" + str(cell)+" \pm "+str(error[i][j])+"}$"+ ending_string
-			else:
-				matrix_string = matrix_string +"$" + str(cell)+" \pm "+str(error[i][j])+"$"+ ending_string
-	column_string = column_string +'''| }'''
-	column_label =""
-	for column in col_labels:
-		column_label = column_label + "&\\textbf{"+column+"}" 
-	latex_string1 = '''\\begin{table}[ht]
+    for i, row in enumerate(mean):
+        column_string = '''{ |c'''
+        matrix_string = matrix_string + \
+            "\\textbf{" + row_labels[i] + "}& "  # length of row labels and number of rows must be equal
+        for j, cell in enumerate(row):
+            column_string = column_string + '''|c'''
+            ending_string = ''' & ''' if j < len(row) - 1 else ''' \\\ \hline
+'''
+            if best_bold_row and cell == min(
+                    row) and best_bold_column == False:
+                matrix_string = matrix_string + \
+                    "$\mathbf{" + str(cell) + " \pm " + str(error[i][j]) + "}$" + ending_string
+            elif best_bold_column and cell == min([a[j] for a in mean]) and best_bold_row == False:
+                matrix_string = matrix_string + \
+                    "$\mathbf{" + str(cell) + " \pm " + str(error[i][j]) + "}$" + ending_string
+            else:
+                matrix_string = matrix_string + "$" + \
+                    str(cell) + " \pm " + str(error[i][j]) + "$" + ending_string
+    column_string = column_string + '''| }'''
+    column_label = ""
+    for column in col_labels:
+        column_label = column_label + "&\\textbf{" + column + "}"
+    latex_string1 = '''\\begin{table}[ht]
 \centering
 \\begin{tabular}
-'''+column_string+'''
+''' + column_string + '''
 \hline
-'''+column_label+"\\\ [0.1ex]"+'''
-'''+matrix_string+'''\end{tabular}
+''' + column_label + "\\\ [0.1ex]" + '''
+''' + matrix_string + '''\end{tabular}
 \\\[-1.5ex]
-\caption{'''+title+'''}
+\caption{''' + title + '''}
 \end{table}'''
-	return latex_string1
+    return latex_string1
 
 
-
-#EXAMPLE
-mean =[[1,6,5,7],[12,4,6,13],[9,8,7,10]]
-error=[[2,6,1,5],[4,8,2,3],[1,4,8,2]]
-
-
-print( latex_matrix_string(mean, error,"Testing Testing", ["row1","row2","row3"], ["col1","col2","col3","col4"]) )
+# EXAMPLE
+mean = [[1, 6, 5, 7], [12, 4, 6, 13], [9, 8, 7, 10]]
+error = [[2, 6, 1, 5], [4, 8, 2, 3], [1, 4, 8, 2]]
 
 
-#TODO: aad robo style docdtrings
+print(
+    latex_matrix_string(
+        mean, error, "Testing Testing", [
+            "row1", "row2", "row3"], [
+                "col1", "col2", "col3", "col4"]))
+
--- plot_mean_std.py
+++ plot_mean_std.py
@@ -1,4 +1,71 @@
-'''
-plot_mean_std
-author: Numair Mansur(numair.mansur@gmail.com)
-'''
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+def plt_mean_std(
+        x,
+        curves,
+        title="",
+        colors=['b', 'g', 'r', 'c', 'm', 'y', 'k'],
+        log_scale_y=False,
+        log_scale_x=False,
+        legend=True):
+    '''
+    Plots Mean and Standard Deviation with an error bar graph
+
+    Parameters
+    ----------
+    x : numpy array
+    	For each curve, contains the x-coordinates. Each entry 
+    	corresponds to one curve.
+    curves : list of numpy arrays 
+    	A list of 2D numpy arrays of mean and standard deviation. First entry in the
+    	numpy array corresponds to the mean and the second entry corresponds to the
+    	error. Each entry in the curves list corresponds to one curve.
+    title : string
+    	Title of the graph
+    colors : string array
+    	Color of the curve. Each entry corresponds to one curve
+    log_scale_y : Boolean
+    	If set to true, changes the y-axis to log scale.
+    log_scale_x: Boolean
+    	If set to true, change the x-axis to log scale.
+    legend : Boolean
+    	If set to true, displays the legend.
+    '''
+    plt.figure()
+    for i, j in enumerate(x):
+        plt.errorbar(j,
+                     curves[i][0],
+                     yerr=curves[i][1],
+                     fmt='o',
+                     label='curve' + str(i + 1),
+                     color=colors[i])
+    plt.title(title)
+    plt.grid()
+    if log_scale_x:
+        plt.xscale('log')
+    if log_scale_y:
+        plt.yscale('log')
+    if legend:
+        plt.legend()
+
+    # Adjust Margins    
+    plot_margin = 0.25
+    x0, x1, y0, y1 = plt.axis()
+    plt.axis((x0 - plot_margin,
+              x1 + plot_margin,
+              y0 - plot_margin,
+              y1 + plot_margin))
+    plt.show()
+
+
+# Example.
+x = np.array([[1, 2, 3, 4], [2, 3, 4, 5], [1, 3, 6, 9], [4, 5, 6, 7]])
+curve1 = np.array([[1, 4, 5, 7], [1, 0.4, 0.7, 0.4]])
+curve2 = np.array([[0.9, 1, 0.7, 0.9], [3, 4, 6, 8]])
+curve3 = np.array([[1.9, -1.1, 2.7, 0.4], [1, 6, 8, 10]])
+curve4 = np.array([[4, 3, 2, 1], [1, 1, 1, 1]])
+curves = [curve1, curve2, curve3, curve4]
+plt_mean_std(x, curves)
