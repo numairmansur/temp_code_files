eb529c97c6307fee30deb00b23255176f92b0d3d
Numair Mansur <numair.mansur@gmail.com>
2016-03-29 01:32:28 +0200

    Create testRfilr.r

---
+++ bio/testRfilr.r
@@ -0,0 +1,10 @@
+library(d3heatmap)
+data <- iris
+data$Species <- NULL
+kc <- kmeans(data,5)
+x <- iris
+list <- kc$cluster
+d <- cbind(x,list)
+sorted <- d[order(d[6]), ]
+d3heatmap(sorted[-5])
+d3heatmap(sorted[1:4])
